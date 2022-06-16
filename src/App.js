be785e42f1bb21630b322590fa6bcd8a0d2396c5
Aidan <abayercalvert@gmail.com>
2022-06-16 14:41:10 -0700

    fixed push err

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Dao from './components/Dao'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Mint from './components/Mint'
+import Help from './components/Help'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dao" element={<Dao />} />
+        <Route path="/mint" element={<Mint />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/* <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Logo from '../assets/logo.png';
+import InstagramLogo from "../assets/6636566_instagram_social media_social network_icon.png";
+import TwitterLogo from "../assets/3225183_app_logo_media_popular_social_icon.png";
+import DiscordLogo from "../assets/3069758_circle_discord_gaming_messenger_round icon_icon.png";
+import OpenseaLogo from '../assets/9255431_opensea_logo_icon.png';
+
+// import { Link } from 'react-scroll';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+      </div>
+
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/dao">Dao</a>
+        </li>
+        <li>
+          <a href="/mint">Mint</a>  
+        </li>
+        <li>
+          <a href="/help">Help</a>  
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <a href="/">Home</a>
+        </li>
+        <li className='py-6 text-4xl'>
+        <a href="/dao">Dao</a>
+        </li>
+        <li className='py-6 text-4xl'>
+        <a href="/mint">Mint</a>
+          
+        </li>
+        <li className='py-6 text-4xl'>
+          <a href="/help">Help</a>
+        </li>
+        <li className='py-6 text-4xl'>
+        <a href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
