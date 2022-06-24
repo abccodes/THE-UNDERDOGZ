c1481b7768b1ca5a587adf7615a4a29b3a1691e1
Aidan <abayercalvert@gmail.com>
2022-06-24 13:34:50 -0700

    background fixed, small uddates

--- src/components/Dao.jsx
+++ src/components/Dao.jsx
@@ -14,14 +14,10 @@ const Dao = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, alias laudantium error tempora veritatis nam fugiat laborum vitae quidem magni praesentium voluptatum animi quaerat quas velit illum minus. Aut, placeat.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo assumenda sed eligendi veniam porro, nisi sunt pariatur voluptatibus explicabo exercitationem rem commodi iure expedita aliquam hic. Quod cumque aliquam eos!</p>  
             </div>
           </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -104,8 +104,29 @@ const Home = () => {
           An investment DAO.
         </h2>
       </div>
-      <div className='roadmap backgroundBlack'> </div>
-      <Roadmap></Roadmap>
+      <div name='about' className='w-full h-screen aboutClass text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, alias laudantium error tempora veritatis nam fugiat laborum vitae quidem magni praesentium voluptatum animi quaerat quas velit illum minus. Aut, placeat.</p>
+            </div>
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo assumenda sed eligendi veniam porro, nisi sunt pariatur voluptatibus explicabo exercitationem rem commodi iure expedita aliquam hic. Quod cumque aliquam eos!</p>  
+            </div>
+          </div>
+      </div>
+    </div>
+      <div className='roadmapClass'>
+      <Roadmap/>
+      </div>
       <div name='skills' className='w-full h-screen  text-gray-300 backgroundBlack'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -150,7 +171,7 @@ const Home = () => {
       </div>
     </div>
     <Header />
-      <div className="faq">
+      <div className="faq ">
         <div className="faqs">
           {faqs.map((faq, i) => (
             <FAQ faq={faq} index={i} toggleFAQ = {toggleFAQ} />
