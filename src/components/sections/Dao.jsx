eaee2d4750731fb65c751703bfbdd53c94b7ce9a
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2022-07-03 23:42:11 -0700

    nft

--- src/components/sections/Dao.jsx
+++ src/components/sections/Dao.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import Nftpuller from '../../n2dpuller/nftpuller.js';
 
 const Dao = () => {
   return (
-    <div name='about' className='w-full h-screen backgroundBlack text-gray-300'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
-              About
-            </p>
-          </div>
-          <div></div>
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, alias laudantium error tempora veritatis nam fugiat laborum vitae quidem magni praesentium voluptatum animi quaerat quas velit illum minus. Aut, placeat.</p>
+    <div name='contact' className='w-full h-screen css-selector flex justify-center items-center p-4'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+          <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+            <div className='sm:text-right pb-8 pl-4'>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+                THE UNDERDOGZ DAO 
+              </p>
             </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo assumenda sed eligendi veniam porro, nisi sunt pariatur voluptatibus explicabo exercitationem rem commodi iure expedita aliquam hic. Quod cumque aliquam eos!</p>  
+            <div></div>
             </div>
-          </div>
-      </div>
-    </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+              <div className='sm:text-right text-4xl font-bold'>
+                <p>Vote - Propose - Earn</p>
+              </div>
+              <div>
+                <p>1 UNDERDOGZ NFT = 1 Vote | 5 UNDERDOGZ NFTs = Proposal Power. Head over to Snapshot Labs to participate, and claim your DIVIDENS when the time comes!</p>  
+              </div>
+            </div>
+        </div>
+        <Nftpuller />
+    </div>  
   );
 };
 
